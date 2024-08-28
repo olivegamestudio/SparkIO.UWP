2f2ed79b4d58f79053f3f99378a7fa538a5680af
Simon Jefferies <simon@olivegame.studio>
2024-08-28 19:56:10 +0100

    UWP variant of SparkIO

---
+++ src/SparkIO.MonoGame/FileSystemMonoGame.cs
@@ -0,0 +1,28 @@
+﻿using System.IO;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+using Microsoft.Xna.Framework;
+using Utility;
+
+namespace SparkIO;
+
+/// <summary>
+/// Defines a set of methods for working with the file system by using MonoGame calls.
+/// </summary>
+public class FileSystemMonoGame : IFileSystem
+{
+    readonly ILogger<FileSystemMonoGame> _logger;
+
+    public FileSystemMonoGame(ILogger<FileSystemMonoGame> logger = null)
+    {
+        _logger = logger;
+    }
+
+    /// <inheritdoc />
+    public Task<Stream> OpenStreamAsync(string filename)
+    {
+        filename = Guard.ThrowIfStringNullOrEmpty(filename);
+        _logger?.LogInformation($"Opening stream: '{filename}'...");
+        return Task.FromResult(TitleContainer.OpenStream(filename));
+    }
+}
---
+++ src/SparkIO.MonoGame/Helpers/ServiceExtensions.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace SparkIO;
+
+public static class ServiceExtensions
+{
+    public static IServiceCollection AddSparkIOForMonoGameUWP(this IServiceCollection services)
+    {
+        services.AddSingleton<IFileSystem, FileSystemMonoGame>();
+        return services;
+    }
+}
